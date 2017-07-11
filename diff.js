2322e2b37779b531d0f240c1255469a35d49bf7e
Elias Ineichen <eineichen@edp.ch>
2017-07-11 18:32:28 +0200

    Abfangen ungueltiger Argumente

---
+++ diff.js
@@ -0,0 +1,18 @@
+// Definition (Deklaration, Initialisierung) der globalen Variablen
+// ...
+
+// CMD-Befehl nach ungueltigen Parametern ueberpruefen
+process.argv.forEach(function (value, index, array) {
+    
+    // Erst ab drittem Argument ueberpruefen, da erste zwei Argumente 'node.js' und 'diff.js' sind
+    if (index > 1) {
+
+        // Wenn Argument NICHT '.json' oder '--save'
+        if (!(value.match(/\.json/) || value === '--save')){
+            console.log("Argument '" + value + "' ungueltig!");
+            process.exit();
+        }
+
+    }
+
+});
